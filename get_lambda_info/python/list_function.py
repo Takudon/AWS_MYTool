9eefc6570416bbbb152ecfb75888b8550a77fb4e
ttanaka <taku.tanaka@omron.com>
2019-10-17 16:07:15 +0900

    Start up the project, whose purpose is collectively manage Lambda inventory tools as python program.

---
+++ get_lambda_info/python/list_function.py
@@ -0,0 +1,73 @@
+############################################################
+# File        : list_functionINFO.py
+# Author      : Taku Tanaka
+# Description :
+#  The python file is storing some modules to get AWS Lambda
+#  usage information. I have used it them when I inventory 
+#  Lambda functions.
+############################################################
+
+
+############################################################
+# Module      : get_session_token
+# Description : 
+#  Get STS session token. 
+#  Using access key, secret access key.
+#  Care for usage of credentials
+############################################################
+def get_session_token():
+    pass
+
+############################################################
+# Module      : get_functions
+# Description :
+#  Get information of AWS Lambda functions.
+#  Return data will be format as dictionally data like json.
+############################################################
+def get_functions():
+    pass
+
+############################################################
+# Module      :
+# Description :
+#  
+#  
+############################################################
+def check_env():
+    pass
+
+############################################################
+# Module      :
+# Description :
+#  
+#  
+############################################################
+def process_functions():
+    pass
+
+############################################################
+# Module      :
+# Description :
+#  
+#  
+############################################################
+def get_latest_execution_time():
+    pass
+
+############################################################
+# Module      :
+# Description :
+#  
+#  
+############################################################
+def format_dataset():
+    pass
+
+############################################################
+# Module      :
+# Description :
+#  
+#  
+############################################################
+def type_dataframe():
+    pass
